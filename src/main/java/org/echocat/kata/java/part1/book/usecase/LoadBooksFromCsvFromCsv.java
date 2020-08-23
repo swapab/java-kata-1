208a3a89f2dc56e51c60426179d938f2345bb5f2
swap <swapnil.abnave@nufin.de>
2020-08-23 12:44:40 +0200

    Load Books from CSV and save in data-store

--- src/main/java/org/echocat/kata/java/part1/book/usecase/LoadBooksFromCsvFromCsv.java
+++ src/main/java/org/echocat/kata/java/part1/book/usecase/LoadBooksFromCsvFromCsv.java
@@ -2,16 +2,23 @@ package org.echocat.kata.java.part1.book.usecase;
 
 import org.echocat.kata.java.part1.util.CsvEntityLoader;
 
+import java.util.Objects;
+
 public class LoadBooksFromCsvFromCsv implements LoadBooksFromCsvUseCase {
     private final CsvEntityLoader csvEntityLoader;
+    private final CreateNewBookUseCase createNewBookUseCase;
 
-    public LoadBooksFromCsvFromCsv(CsvEntityLoader csvEntityLoader) {
+    public LoadBooksFromCsvFromCsv(CsvEntityLoader csvEntityLoader,
+                                   CreateNewBookUseCase createNewBookUseCase) {
         this.csvEntityLoader = csvEntityLoader;
+        this.createNewBookUseCase = createNewBookUseCase;
     }
 
     @Override
     public void loadBooks() {
-        // load books from csv
-        // create books
+        ClassLoader classLoader = getClass().getClassLoader();
+        csvEntityLoader.loadBooks(
+                Objects.requireNonNull(classLoader.getResource("org/echocat/kata/java/part1/data/books.csv")).getFile())
+                       .forEach(createNewBookUseCase::saveBook);
     }
 }
--- src/test/java/org/echocat/kata/java/part1/book/usecase/LoadBooksFromCsvFromCsvTest.java
+++ src/test/java/org/echocat/kata/java/part1/book/usecase/LoadBooksFromCsvFromCsvTest.java
@@ -1,24 +1,42 @@
 package org.echocat.kata.java.part1.book.usecase;
 
+import org.echocat.kata.java.part1.book.domain.Book;
 import org.echocat.kata.java.part1.util.CsvEntityLoader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class LoadBooksFromCsvFromCsvTest {
 
     private CsvEntityLoader mockCsvEntityLoader = mock(CsvEntityLoader.class);
+    private CreateNewBookUseCase mockCreateNewBookUseCase = mock(CreateNewBookUseCase.class);
 
     private LoadBooksFromCsvFromCsv loadBooksFromCsvFromCsv;
 
     @BeforeEach
     void setUp() {
-        loadBooksFromCsvFromCsv = new LoadBooksFromCsvFromCsv(mockCsvEntityLoader);
+        loadBooksFromCsvFromCsv = new LoadBooksFromCsvFromCsv(mockCsvEntityLoader, mockCreateNewBookUseCase);
     }
 
     @Test
     void shouldCreate_N_BooksFromCsvWith_N_Records() {
+        Book book = Book.builder().build();
+        List<Book> books = List.of(book);
+        Path resourceDirectory = Paths.get("target", "classes");
+        resourceDirectory.toAbsolutePath();
+        String expectedBookCsvFilePath = resourceDirectory.toAbsolutePath() + "/org/echocat/kata/java/part1/data/books.csv";
+        given(mockCsvEntityLoader.loadBooks(expectedBookCsvFilePath)).willReturn(books);
+
+        loadBooksFromCsvFromCsv.loadBooks();
+
+        verify(mockCreateNewBookUseCase).saveBook(book);
     }
 }
 
